3ed11f2cc2c65a3e293f2bac192896d99a38c3e0
rkuhl <alightinmyhair@gmail.com>
2019-12-08 14:58:16 -0600

    Setup Support Classes successfully

--- src/org/launchcode/techjobs_oo/CoreCompetency.java
+++ src/org/launchcode/techjobs_oo/CoreCompetency.java
@@ -32,6 +32,18 @@ public class CoreCompetency {
         return id == that.id;
     }
 
+    public int getId() {
+        return id;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
     @Override
     public int hashCode() {
         return Objects.hash(id);
@@ -39,4 +51,7 @@ public class CoreCompetency {
 
     // TODO: Use the "Generate" tool to add a getter and setter for the 'value' field but
     //  ONLY a getter for the 'id' field.
+    // DONE WITH TO-DO
+
+
 }
